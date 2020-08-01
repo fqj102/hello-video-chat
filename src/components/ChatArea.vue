d03dbccf6c8a3635c2a549dad25beab6b044bd9a
cheonggil.bang <fqj102@gmail.com>
2020-08-01 16:40:50 +0900

    fix commit

--- src/components/ChatArea.vue
+++ src/components/ChatArea.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="message">
-    <div v-for="msg in messages" :key="msg.msg" class="message__container">
+    <div v-for="(msg,index) in messages" :key="index" class="message__container">
       <p
         v-if="!msg.join"
         class="message__text"
         :class="{ own: msg.isMe, other: !msg.isMe}"
         v-message="msg.msg"
       ></p>
-      <p 
+      <p
         v-if="msg.join"
-        class="message__joined"> 
+        class="message__joined">
         {{msg.msg}}</p>
     </div>
   </div>
@@ -33,7 +33,7 @@ export default {
 
         if(isObj) {
           chunks = Math.ceil(binding.value.message.length / maxLength)
-          el.innerHTML = `<span style="font-weight:bold">${binding.value.username}</span>: 
+          el.innerHTML = `<span style="font-weight:bold">${binding.value.username}</span>:
             ${vnode.context.getChunkText(binding.value.message, maxLength, chunks)}`
         } else {
           chunks = Math.ceil(binding.value.length / maxLength)
